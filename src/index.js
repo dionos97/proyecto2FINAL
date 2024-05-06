818071137eccdae0fe7827ba976c30f4e19b2ab2
dionos97 <munoz.diego89@hotmail.com>
2024-05-06 16:08:12 -0500

    terecer commit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
-import express from 'express'
-import { swaggerDocs } from './config/swagger.js'
-import userRoutes from './routes/user.routes.js'
-import postRoutes from './routes/post.routes.js'
-import categoryRoutes from './routes/category.routes.js'
-import commentsRoutes from './routes/comments.routes.js'
+import express from 'express';
+import { swaggerDocs } from './config/swagger.js';
+import userRoutes from './routes/user.routes.js';
+import postRoutes from './routes/post.routes.js';
+import categoryRoutes from './routes/category.routes.js';
+import commentsRoutes from './routes/comments.routes.js';
+import YAML from 'yamljs';
+import swaggerUi from 'swagger-ui-express';
 
 const PORT = process.env.PORT || 3000; // Definir el puerto aquí
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(userRoutes)
-app.use(postRoutes)
-app.use(categoryRoutes)
-app.use(commentsRoutes)
+app.use(userRoutes);
+app.use(postRoutes);
+app.use(categoryRoutes);
+app.use(commentsRoutes);
 
-swaggerDocs(app)
+const swaggerDocument = YAML.load('./docs/openapi.yaml');
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
+// Middleware para servir la documentación de OpenAPI
+app.use('/api-docs', swaggerUi.serve);
+app.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
